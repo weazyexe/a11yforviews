37cacf097c0b22f933097c0608c1a324c6582624
Timur Zadvornov <weazy.exe@gmail.com>
2022-06-14 16:47:08 +0400

    added custom attrs to the wallet card

--- app/src/main/java/dev/weazyexe/a11yforviews/customviews/WalletCard.kt
+++ app/src/main/java/dev/weazyexe/a11yforviews/customviews/WalletCard.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
+import dev.weazyexe.a11yforviews.R
 import dev.weazyexe.a11yforviews.databinding.ViewWalletCardBinding
 
 class WalletCard @JvmOverloads constructor(
@@ -14,4 +16,16 @@ class WalletCard @JvmOverloads constructor(
     private val binding = ViewWalletCardBinding.inflate(
         LayoutInflater.from(context), this, true
     )
+
+    init {
+        parseAttributes(attrs)
+    }
+
+    private fun parseAttributes(attrs: AttributeSet?) = with(binding) {
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.WalletCard)
+        titleTv.text = attributes.getString(R.styleable.WalletCard_wallet_card_title)
+        balanceTv.text = attributes.getString(R.styleable.WalletCard_wallet_card_balance)
+        addressTv.text = attributes.getString(R.styleable.WalletCard_wallet_card_address)
+        attributes.recycle();
+    }
 }
