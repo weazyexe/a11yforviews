03226fcb38bb1be42ca98eb2c30fd936a2ef1269
Timur Zadvornov <weazy.exe@gmail.com>
2022-06-14 18:22:52 +0400

    a11y was supported on wallet card

--- app/src/main/java/dev/weazyexe/a11yforviews/customviews/WalletCard.kt
+++ app/src/main/java/dev/weazyexe/a11yforviews/customviews/WalletCard.kt
@@ -3,8 +3,12 @@ package dev.weazyexe.a11yforviews.customviews
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
-import androidx.core.content.ContextCompat
+import android.widget.Toast
+import androidx.core.view.AccessibilityDelegateCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
 import dev.weazyexe.a11yforviews.R
 import dev.weazyexe.a11yforviews.databinding.ViewWalletCardBinding
 
@@ -21,11 +25,70 @@ class WalletCard @JvmOverloads constructor(
         parseAttributes(attrs)
     }
 
-    private fun parseAttributes(attrs: AttributeSet?) = with(binding) {
+    private fun parseAttributes(attrs: AttributeSet?) {
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.WalletCard)
-        titleTv.text = attributes.getString(R.styleable.WalletCard_wallet_card_title)
-        balanceTv.text = attributes.getString(R.styleable.WalletCard_wallet_card_balance)
-        addressTv.text = attributes.getString(R.styleable.WalletCard_wallet_card_address)
-        attributes.recycle();
+        val title = attributes.getString(R.styleable.WalletCard_wallet_card_title).orEmpty()
+        val balance = attributes.getString(R.styleable.WalletCard_wallet_card_balance).orEmpty()
+        val address = attributes.getString(R.styleable.WalletCard_wallet_card_address).orEmpty()
+        attributes.recycle()
+
+        setupAccessibility()
+        setData(title, balance, address)
+    }
+
+    fun setData(
+        title: String,
+        balance: String,
+        address: String
+    ) = with(binding) {
+        titleTv.text = title
+        balanceTv.text = balance
+        addressTv.text = address
+        // Строка со значением "Wallet card: %s, balance is %s"
+        root.contentDescription = context.getString(
+            R.string.wallet_card_accessibility_description,
+            title,
+            balance
+        )
+    }
+
+    private fun setupAccessibility() {
+        ViewCompat.setAccessibilityDelegate(
+            binding.root,
+            object : AccessibilityDelegateCompat() {
+                override fun onInitializeAccessibilityNodeInfo(
+                    host: View,
+                    info: AccessibilityNodeInfoCompat
+                ) {
+                    super.onInitializeAccessibilityNodeInfo(host, info)
+                    val clickAction = AccessibilityNodeInfoCompat.AccessibilityActionCompat(
+                        AccessibilityNodeInfoCompat.ACTION_CLICK,
+                        // Строка со значением "View wallet details"
+                        context.getString(R.string.wallet_card_accessibility_card_click_action)
+                    )
+                    info.addAction(clickAction)
+                }
+            }
+        )
+
+        ViewCompat.addAccessibilityAction(
+            binding.root,
+            // Строка "Show address QR code"
+            context.getString(R.string.wallet_card_accessibility_show_qr_code_action)
+        ) { _, _ ->
+            // Открываем диалог с QR здесь
+            Toast.makeText(context, "QR code has shown", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        ViewCompat.addAccessibilityAction(
+            binding.root,
+            // Строка "Copy the wallet address"
+            context.getString(R.string.wallet_card_accessibility_copy_the_address_action)
+        ) { _, _ ->
+            // Кладём строку с адресом в буфер обмена
+            Toast.makeText(context, "Address has copied", Toast.LENGTH_SHORT).show()
+            true
+        }
     }
 }
