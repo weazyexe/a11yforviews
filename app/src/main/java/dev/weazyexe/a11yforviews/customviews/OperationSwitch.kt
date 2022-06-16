17c1c5378d4c56ee58b8e51c01719e0a46a41380
Timur Zadvornov <weazy.exe@gmail.com>
2022-06-16 17:44:48 +0400

    added operation switch accessibility

--- app/src/main/java/dev/weazyexe/a11yforviews/customviews/OperationSwitch.kt
+++ app/src/main/java/dev/weazyexe/a11yforviews/customviews/OperationSwitch.kt
@@ -3,9 +3,14 @@ package dev.weazyexe.a11yforviews.customviews
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
+import androidx.core.view.AccessibilityDelegateCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
 import com.google.android.material.color.MaterialColors
+import dev.weazyexe.a11yforviews.R
 import dev.weazyexe.a11yforviews.databinding.ViewOperationSwitchBinding
 import com.google.android.material.R as materialR
 
@@ -21,26 +26,71 @@ class OperationSwitch @JvmOverloads constructor(
 
     init {
         initListeners()
-        binding.depositButton.select()
+        binding.depositButton.performClick()
+        setupAccessibility()
     }
 
     private fun initListeners() = with(binding) {
         depositButton.setOnClickListener {
             depositButton.select()
             transferButton.deselect()
+            updateStateDescription()
         }
 
         transferButton.setOnClickListener {
             transferButton.select()
             depositButton.deselect()
+            updateStateDescription()
         }
+    }
+
+    private fun updateStateDescription() {
+        ViewCompat.setStateDescription(
+            binding.root,
+            context.getString(
+                if (binding.transferButton.isSelected) {
+                    R.string.operation_switch_transfer_label
+                } else {
+                    R.string.operation_switch_deposit_label
+                }
+            )
+        )
+    }
+
+    private fun setupAccessibility() = with(binding) {
+        ViewCompat.replaceAccessibilityAction(
+            root,
+            AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_CLICK,
+            // Строка "Toggle"
+            context.getString(R.string.operation_switch_accessibility_toggle_action)
+        ) { _, _ ->
+            if (transferButton.isSelected) {
+                depositButton.performClick()
+            } else {
+                transferButton.performClick()
+            }
+            true
+        }
+
+        ViewCompat.setAccessibilityDelegate(
+            root,
+            object : AccessibilityDelegateCompat() {
+                override fun onInitializeAccessibilityNodeInfo(
+                    host: View,
+                    info: AccessibilityNodeInfoCompat
+                ) {
+                    super.onInitializeAccessibilityNodeInfo(host, info)
+                    info.roleDescription = context.getString(R.string.role_description_switch)
+                }
+            }
+        )
     }
 
     private fun Button.select() {
         isSelected = true
         setTextColor(
             MaterialColors.getColor(
-                this@OperationSwitch,
+                this,
                 materialR.attr.colorOnPrimary
             )
         )
@@ -50,7 +100,7 @@ class OperationSwitch @JvmOverloads constructor(
         isSelected = false
         setTextColor(
             MaterialColors.getColor(
-                this@OperationSwitch,
+                this,
                 materialR.attr.colorPrimary
             )
         )
